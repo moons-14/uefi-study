1fa423c35b0bbfd79aaa9e34029ff0920e8b8bab
moons14 <moons@moons14.com>
2024-07-30 19:15:14 +0900

    fix

--- shell.c
+++ shell.c
@@ -1,21 +1,20 @@
 #include "common.h"
 #include "shell.h"
 
-#define MAX_COMMAND_LEN 100
+#define MAX_COMMAND_LEN	100
 
 void shell(void)
 {
-    unsigned short cmd[MAX_COMMAND_LEN];
+	unsigned short com[MAX_COMMAND_LEN];
 
-    while (TRUE)
-    {
-        puts(L"poiOS> ");
-        if(gets(cmd,MAX_COMMAND_LEN)<=0)
-            continue;
-        if(!strcmp(L"hello",cmd))
-            puts(L"Hello UEFI!\n");
-        else
-            puts(L"Command not found.\n");
-    }
-    
+	while (TRUE) {
+		puts(L"poiOS> ");
+		if (gets(com, MAX_COMMAND_LEN) <= 0)
+			continue;
+
+		if (!strcmp(L"hello", com))
+			puts(L"Hello UEFI!\r\n");
+		else
+			puts(L"Command not found.\r\n");
+	}
 }
